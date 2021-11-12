c14df118e1507f2fdee42ae1dd29949f5aa94077
js6pak <kubastaron@hotmail.com>
2021-11-12 13:15:50 +0100

    DisableEndGame

---
+++ Laboratory.Mods/GameConfig.cs
@@ -0,0 +1,6 @@
+namespace Laboratory.Mods;
+
+public static class GameConfig
+{
+    public static bool DisableEndGame { get; set; } = true;
+}
---
+++ Laboratory.Mods/Patches/EndGamePatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace Laboratory.Mods.Patches
+{
+    [HarmonyPatch]
+    internal static class EndGamePatches
+    {
+        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CheckEndCriteria))]
+        [HarmonyPrefix]
+        public static bool CheckEndCriteriaPatch() => !GameConfig.DisableEndGame;
+
+        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CheckTaskCompletion))]
+        [HarmonyPrefix]
+        public static bool CheckTaskCompletionPatch() => !GameConfig.DisableEndGame;
+
+        [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.IsGameOverDueToDeath))]
+        [HarmonyPostfix]
+        public static void IsGameOverDueToDeathPatch(ref bool __result)
+        {
+            __result = !GameConfig.DisableEndGame && __result;
+        }
+    }
+}
