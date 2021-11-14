5789bc2a0f2e3d8850ceb5eccf64d233b31bfa35
Simon Kelly <simonkellly@gmail.com>
2021-11-14 21:01:24 +0000

    Add additional game config options

--- Laboratory/GameConfig.cs
+++ Laboratory/GameConfig.cs
@@ -3,4 +3,7 @@ namespace Laboratory;
 public static class GameConfig
 {
     public static bool DisableEndGame { get; set; } = true;
+    public static bool DisableMeetings { get; set; } = true;
+    public static bool DisableKillButton { get; set; } = true;
+    public static bool DisableTaskArrows { get; set; } = true;
 }
---
+++ Laboratory/Patches/GameConfigPatches.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Laboratory.Patches;
+
+[HarmonyPatch]
+internal static class GameConfigPatches
+{
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CheckEndCriteria))]
+    [HarmonyPrefix]
+    public static bool CheckEndCriteriaPatch() => !GameConfig.DisableEndGame;
+
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.CheckTaskCompletion))]
+    [HarmonyPrefix]
+    public static bool CheckTaskCompletionPatch() => !GameConfig.DisableEndGame;
+
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.IsGameOverDueToDeath))]
+    [HarmonyPostfix]
+    public static void IsGameOverDueToDeathPatch(ref bool __result)
+    {
+        __result = !GameConfig.DisableEndGame && __result;
+    }
+    
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CmdReportDeadBody))]
+    [HarmonyPrefix]
+    public static bool CmdReportDeadBodyPatch()
+    {
+        return !GameConfig.DisableMeetings;
+    }
+    
+    [HarmonyPatch(typeof(KillButtonManager), nameof(KillButtonManager.PerformKill))]
+    [HarmonyPrefix]
+    public static bool PerformKillPatch()
+    {
+        return !GameConfig.DisableKillButton;
+    }
+    
+    [HarmonyPatch(typeof(KillButtonManager), nameof(KillButtonManager.SetTarget))]
+    [HarmonyPrefix]
+    public static bool SetTargetPatch()
+    {
+        return !GameConfig.DisableKillButton;
+    }
+    
+    [HarmonyPatch(typeof(ArrowBehaviour), nameof(ArrowBehaviour.Awake))]
+    [HarmonyPostfix]
+    public static void ArrowBehaviourAwakePatch(ArrowBehaviour __instance)
+    {
+        if (!GameConfig.DisableTaskArrows || !__instance.image) return;
+        __instance.image.sprite = null;
+    }
+}
