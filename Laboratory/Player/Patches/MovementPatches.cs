144aeaa7ec2867379f15529750529f1b5b9df63f
Simon Kelly <simonkellly@gmail.com>
2021-11-19 09:47:08 +0000

    Use playerphysics speed in customnetworktransform

---
+++ Laboratory/Player/Patches/MovementPatches.cs
@@ -0,0 +1,29 @@
+using HarmonyLib;
+using Laboratory.Extensions;
+
+namespace Laboratory.Player.Patches;
+
+[HarmonyPatch]
+internal static class MovementPatches
+{
+    public static PlayerPhysics? LastPhysics = null;
+    
+    [HarmonyPatch(typeof(CustomNetworkTransform), nameof(CustomNetworkTransform.FixedUpdate))]
+    [HarmonyPrefix]
+    public static void PrepareForChangingTrueSpeed(CustomNetworkTransform __instance)
+    {
+        LastPhysics = __instance.GetCachedComponent<PlayerPhysics>();
+    }
+    
+    [HarmonyPatch(typeof(PlayerPhysics), nameof(PlayerPhysics.TrueSpeed), MethodType.Getter)]
+    [HarmonyPrefix, HarmonyPriority(Priority.First)]
+    public static bool GetActualTrueSpeed(ref float __result)
+    {
+        if (LastPhysics == null) return true;
+            
+        PlayerPhysics physics = LastPhysics;
+        LastPhysics = null;
+        __result = physics.TrueSpeed;
+        return false;
+    }
+}
