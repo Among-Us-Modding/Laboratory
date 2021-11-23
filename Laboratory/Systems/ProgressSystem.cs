9aef19b65f23685668e9e51f69f1d364b3c1601c
Simon Kelly <simonkellly@gmail.com>
2021-11-23 01:01:26 +0000

    Add ProgressSystem to laboratory

---
+++ Laboratory/Systems/ProgressSystem.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Linq;
+using Hazel;
+using Laboratory.CustomMap;
+using Reactor;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using IntPtr = System.IntPtr;
+using Object = Il2CppSystem.Object;
+
+namespace Laboratory.Systems;
+
+[RegisterInIl2Cpp(typeof(ISystemType))]
+public class ProgressSystem : Object, ICustomSystemType
+{
+    private static ProgressSystem? _instance;
+    public static ProgressSystem? Instance => ShipStatus.Instance ? _instance : null;
+    public static CustomSystemType SystemType { get; } = CustomSystemType.Register<ProgressSystem>();
+    public static float[] Stages { get; } = {
+        60f,
+        60f,
+        60f,
+        60f,
+        60f,
+        60f,
+        60f,
+        60f,
+        60f,
+        60f,
+    };
+    
+    public ProgressSystem(IntPtr ptr) : base(ptr)
+    {
+    }
+
+    public ProgressSystem() : base(ClassInjector.DerivedConstructorPointer<ProgressSystem>())
+    {
+        ClassInjector.DerivedConstructorBody(this);
+        _instance = this;
+    }
+
+    public float Timer { get; set; }
+    public bool IsDirty { get; set; }
+
+    private float _totalTime = -1;
+    public float TotalTime => _totalTime < 0 ? _totalTime = Stages.Sum() : _totalTime;
+
+    private int _stage;
+    public int Stage
+    {
+        get => _stage;
+        set
+        {
+            _stage = value;
+            IsDirty = true;
+        }
+    }
+    
+    public float Progress
+    {
+        get
+        {
+            int stageLength = Stages.Length;
+            return Stage == stageLength ? 1 : Mathf.Clamp01((Timer - Stages.Take(Stage).Sum()) / (Stages[Stage] * stageLength) + (float) Stage / stageLength);
+        }
+    }
+    
+    public void Detoriorate(float deltaTime)
+    {
+        Timer += deltaTime;
+        Timer = Mathf.Clamp(Timer + deltaTime, 0, TotalTime);
+        if (!AmongUsClient.Instance.AmHost) return;
+        float sum = 0;
+        int stage = 0;
+        for (int i = 0; i < Stages.Length; i++)
+        {
+            sum += Stages[i];
+            if (Timer >= sum) stage++;
+        }
+
+        if (Stage != stage) Stage = stage;
+    }
+    
+    public void Serialize(MessageWriter writer, bool initialState)
+    {
+        writer.Write((byte)Stage);
+        writer.Write(Timer);
+        IsDirty = false;
+    }
+
+    public void Deserialize(MessageReader reader, bool initialState)
+    {
+        Stage = reader.ReadByte();
+        Timer = reader.ReadSingle();
+    }
+
+    public void RepairDamage(PlayerControl player, byte amount)
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void UpdateSystem(PlayerControl player, MessageReader msgReader)
+    {
+        throw new InvalidOperationException();
+    }
+}
