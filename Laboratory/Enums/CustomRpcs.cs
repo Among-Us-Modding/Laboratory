9ea928935c60e5c356d72ff67d3ad06aaae53396
Simon Kelly <simonkellly@gmail.com>
2021-11-23 01:21:33 +0000

    Add CounterSystem

--- Laboratory/Enums/CustomRpcs.cs
+++ Laboratory/Enums/CustomRpcs.cs
@@ -8,4 +8,6 @@ public enum CustomRpcs : uint
     HardReset,
     ResetMovement,
     EndGame,
+    SetCount,
+    ChangeCount,
 }
---
+++ Laboratory/Systems/CounterSystem.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hazel;
+using Laboratory.CustomMap;
+using Laboratory.Enums;
+using Reactor;
+using Reactor.Networking.MethodRpc;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using IntPtr = System.IntPtr;
+using Object = Il2CppSystem.Object;
+
+namespace Laboratory.Systems;
+
+[RegisterInIl2Cpp(typeof(ISystemType))]
+public class CounterSystem : Object, ICustomSystemType
+{
+    public static int GetCount(uint key)
+    {
+        Instance!.Counters.TryGetValue(key, out var res);
+        return res;
+    }
+
+    [MethodRpc((uint)CustomRpcs.SetCount)]
+    public static void SetCount(uint key, int value)
+    {
+        if (AmongUsClient.Instance.AmHost)
+        {
+            Instance!.Counters[key] = value;
+            Instance.IsDirty = true;
+        }
+    }
+
+    [MethodRpc((uint)CustomRpcs.ChangeCount)]
+    public static void ChangeCount(uint key, int change)
+    {
+        if (AmongUsClient.Instance.AmHost)
+        {
+            SetCount(key, GetCount(key) + change);
+        }
+    }
+    
+    private static CounterSystem? _instance;
+    public static CounterSystem? Instance => ShipStatus.Instance ? _instance : null;
+    
+    public static CustomSystemType SystemType { get; } = CustomSystemType.Register<ProgressSystem>();
+
+    public CounterSystem(IntPtr ptr) : base(ptr)
+    {
+    }
+
+    public CounterSystem() : base(ClassInjector.DerivedConstructorPointer<ProgressSystem>())
+    {
+        ClassInjector.DerivedConstructorBody(this);
+        _instance = this;
+    }
+    
+    public bool IsDirty { get; set; }
+    public Dictionary<uint, int> Counters { get; } = new();
+    
+    public void Serialize(MessageWriter writer, bool initialState)
+    {
+        writer.Write(Counters.Count);
+        foreach ((uint key, int value) in Counters)
+        {
+            writer.Write(key);
+            writer.Write(value);
+        }
+
+        IsDirty = false;
+    }
+
+    public void Deserialize(MessageReader reader, bool initialState)
+    {
+        Counters.Clear();
+        var count = reader.ReadInt32();
+        for (int i = 0; i < count; i++)
+        {
+            Counters[reader.ReadUInt32()] = reader.ReadInt32();
+        }
+    }
+    
+    public void Detoriorate(float deltaTime) { }
+
+    public void RepairDamage(PlayerControl player, byte amount)
+    {
+        throw new InvalidOperationException();
+    }
+
+    public void UpdateSystem(PlayerControl player, MessageReader msgReader)
+    {
+        throw new InvalidOperationException();
+    }
+}
