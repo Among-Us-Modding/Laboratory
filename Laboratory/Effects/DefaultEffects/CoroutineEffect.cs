fbe7c60f9dc17701747bb3de1095cc32d07b67f0
Simon Kelly <simonkellly@gmail.com>
2022-02-28 18:58:43 +0000

    Create CoroutineEffect

---
+++ Laboratory/Effects/DefaultEffects/CoroutineEffect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using BepInEx.IL2CPP.Utils;
+using Laboratory.Player.Managers;
+using UnityEngine;
+
+namespace Laboratory.Effects.DefaultEffects;
+
+public abstract class CoroutineEffect : IPlayerEffect
+{
+    public float Timer { get; set; } = 1;
+    public PlayerManager Owner { get; set; }
+    public Coroutine EffectRoutine { get; set; }
+    public virtual void Awake()
+    {
+        EffectRoutine = Owner.StartCoroutine(CoEffect());
+    }
+
+    public abstract IEnumerator CoEffect();
+    
+    public virtual void Cancel()
+    {
+        Owner.StopCoroutine(EffectRoutine);
+    }
+}
