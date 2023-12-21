3af97c56ad647ae64af1af931cb53ae1d323b3eb
Simon Kelly <simonkellly@gmail.com>
2023-12-21 23:07:29 +0000

    Update ShipStatus_Patches.cs

--- Laboratory/Systems/Patches/ShipStatus_Patches.cs
+++ Laboratory/Systems/Patches/ShipStatus_Patches.cs
@@ -2,10 +2,83 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
+using Hazel;
 using Il2CppInterop.Runtime.InteropTypes;
 using Laboratory.Map;
+using UnityEngine;
+using Object = Il2CppSystem.Object;
 
 namespace Laboratory.Systems.Patches;
+
+[HarmonyPatch]
+public static class SystemPatches
+{
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.Serialize))]
+    [HarmonyPrefix]
+    public static bool Serialize(ShipStatus __instance, MessageWriter writer, bool initialState, out bool __result)
+    {
+        bool result = false;
+        for (short num = 0; num < SystemTypeHelpers.AllTypes.Length; num++)
+        {
+            SystemTypes systemTypes = SystemTypeHelpers.AllTypes[num];
+            if (__instance.Systems.TryGetValue(systemTypes, out var value) && (initialState || value.IsDirty))
+            {
+                result = true;
+                writer.StartMessage((byte)systemTypes);
+                if (value.TryCast<Object>() is ICustomSystemType sys) sys.Serialize(writer, initialState);
+                else value.Serialize(writer, initialState);
+                writer.EndMessage();
+            }
+        }
+        __result = result;
+        return false;
+    }
+
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.Deserialize))]
+    [HarmonyPrefix]
+    public static void Deserialize(ShipStatus __instance, MessageReader reader, bool initialState)
+    {
+        while (reader.Position < reader.Length)
+        {
+            MessageReader messageReader = reader.ReadMessage();
+            SystemTypes key = (SystemTypes)messageReader.Tag;
+            if (__instance.Systems.TryGetValue(key, out var value))
+            {
+                if (value.TryCast<Object>() is ICustomSystemType sys) sys.Deserialize(messageReader, initialState);
+                else value.Deserialize(messageReader, initialState);
+            }
+        }
+    }
+    
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.FixedUpdate))]
+    [HarmonyPrefix]
+    public static void FixedUpdate(ShipStatus __instance)
+    {
+        if (!AmongUsClient.Instance || !PlayerControl.LocalPlayer)
+        {
+            return;
+        }
+        __instance.Timer += Time.fixedDeltaTime;
+        __instance.EmergencyCooldown -= Time.fixedDeltaTime;
+        if ((bool)GameData.Instance)
+        {
+            GameData.Instance.RecomputeTaskCounts();
+        }
+        if (!AmongUsClient.Instance.AmClient)
+        {
+            return;
+        }
+        for (int i = 0; i < SystemTypeHelpers.AllTypes.Length; i++)
+        {
+            SystemTypes key = SystemTypeHelpers.AllTypes[i];
+            if (__instance.Systems.TryGetValue(key, out var value))
+            {
+                if (value.TryCast<Object>() is ICustomSystemType sys) sys.Detoriorate(Time.fixedDeltaTime);
+                else value.Deteriorate(Time.fixedDeltaTime);
+            }
+        }
+    }
+}
 
 [HarmonyPatch]
 public static class ShipStatus_OnEnable_Patch
@@ -15,6 +88,7 @@ public static class ShipStatus_OnEnable_Patch
         yield return AccessTools.Method(typeof(ShipStatus), nameof(ShipStatus.OnEnable));
         yield return AccessTools.Method(typeof(PolusShipStatus), nameof(PolusShipStatus.OnEnable));
         yield return AccessTools.Method(typeof(AirshipStatus), nameof(AirshipStatus.OnEnable));
+        yield return AccessTools.Method(typeof(FungleShipStatus), nameof(FungleShipStatus.OnEnable));
     }
 
     public static void Prefix(ShipStatus __instance, out bool __state)
